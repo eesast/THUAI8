da19ba0ee797eb45df591ea883597e0fe679b391
Henry <2280825018@qq.com>
2024-10-24 16:47:29 +0800

    feat: :sparkles: Logic Update

---
+++ logic/Preparation/Interface/IOccupation.cs
@@ -0,0 +1,13 @@
+﻿using Preparation.Utility;
+
+namespace Preparation.Interface;
+
+public interface IOccupation
+{
+    public int Cost { get; }
+    public int MoveSpeed { get; }
+    public int MaxHp { get; }
+    public int BaseAttackSize { get; }
+    public int ViewRange { get; }
+    public bool IsModuleValid(ModuleType moduleType);
+}
