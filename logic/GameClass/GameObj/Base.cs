eb0c2800b7a19fb291fb0ca9c0cf0dbae3e57dec
Henry <2280825018@qq.com>
2025-03-01 11:28:07 +0800

    fix: debug

--- logic/GameClass/GameObj/Base.cs
+++ logic/GameClass/GameObj/Base.cs
@@ -17,18 +17,15 @@ namespace GameClass.GameObj
         public ObjPool<Character, CharacterType> CharacterPool { get; }
         private List<XY> birthPointList = [];//根据兵营初始化
         public List<XY> BirthPointList => birthPointList;
-        public Home Home { get; set; }
         public MoneyPool MoneyPool { get; } = new();
         public AtomicInt BarrackNum { get; } = new(0);
         public AtomicInt FarmNum { get; } = new(1);
         public int MoneyAddPerSecond => FarmNum * GameData.ScoreFarmPerSecond;
-        public Base(Construction construction)
+        public Base(long teamID)
         {
-            TeamID = new(construction.TeamID);
-            Construction = construction;
+            TeamID = new(teamID);
             CharacterPool = new(
                 classfier: (character) => character.CharacterType,//以下可能出问题
-                idleChecker: (character) => character.IsRemoved,
                 idleChecker: (character) => character.IsRemoved,
                 tryActivator: (character) =>
                 {
@@ -46,28 +43,28 @@ namespace GameClass.GameObj
                     character.IsRemoved.SetROri(true);
                 });
             // 池初始化，但是由于服务器采用逐个添加船只的方式，因此这里不进行任何行为
-            CharacterPool.Initiate(CharacterType.Tangseng, 0,
-                              () => new(GameData.CharacterRadius, CharacterType.Tangseng, MoneyPool));
-            CharacterPool.Initiate(CharacterType.Sunwukong, 0,
-                              () => new(GameData.CharacterRadius, CharacterType.Sunwukong, MoneyPool));
-            CharacterPool.Initiate(CharacterType.Zhubajie, 0,
-                              () => new(GameData.CharacterRadius, CharacterType.Zhubajie, MoneyPool));
-            CharacterPool.Initiate(CharacterType.Shawujing, 0,
-                              () => new(GameData.CharacterRadius, CharacterType.Shawujing, MoneyPool));
-            CharacterPool.Initiate(CharacterType.Bailongma, 0,
-                              () => new(GameData.CharacterRadius, CharacterType.Bailongma, MoneyPool));
+            CharacterPool.Initiate(CharacterType.TangSeng, 0,
+                              () => new(GameData.CharacterRadius, CharacterType.TangSeng, MoneyPool));
+            CharacterPool.Initiate(CharacterType.SunWukong, 0,
+                              () => new(GameData.CharacterRadius, CharacterType.SunWukong, MoneyPool));
+            CharacterPool.Initiate(CharacterType.ZhuBajie, 0,
+                              () => new(GameData.CharacterRadius, CharacterType.ZhuBajie, MoneyPool));
+            CharacterPool.Initiate(CharacterType.ShaWujing, 0,
+                              () => new(GameData.CharacterRadius, CharacterType.ShaWujing, MoneyPool));
+            CharacterPool.Initiate(CharacterType.BaiLongma, 0,
+                              () => new(GameData.CharacterRadius, CharacterType.BaiLongma, MoneyPool));
             CharacterPool.Initiate(CharacterType.Monkid, 0,
                               () => new(GameData.CharacterRadius, CharacterType.Monkid, MoneyPool));
-            CharacterPool.Initiate(CharacterType.Jiuling, 0,
-                              () => new(GameData.CharacterRadius, CharacterType.Jiuling, MoneyPool));
-            CharacterPool.Initiate(CharacterType.Honghaier, 0,
-                              () => new(GameData.CharacterRadius, CharacterType.Honghaier, MoneyPool));
-            CharacterPool.Initiate(CharacterType.Niumowang, 0,
-                              () => new(GameData.CharacterRadius, CharacterType.Niumowang, MoneyPool));
-            CharacterPool.Initiate(CharacterType.Tieshan, 0,
-                              () => new(GameData.CharacterRadius, CharacterType.Tieshan, MoneyPool));
-            CharacterPool.Initiate(CharacterType.Zhizhujing, 0,
-                              () => new(GameData.CharacterRadius, CharacterType.Zhizhujing, MoneyPool));
+            CharacterPool.Initiate(CharacterType.JiuLing, 0,
+                              () => new(GameData.CharacterRadius, CharacterType.JiuLing, MoneyPool));
+            CharacterPool.Initiate(CharacterType.HongHaier, 0,
+                              () => new(GameData.CharacterRadius, CharacterType.HongHaier, MoneyPool));
+            CharacterPool.Initiate(CharacterType.NiuMowang, 0,
+                              () => new(GameData.CharacterRadius, CharacterType.NiuMowang, MoneyPool));
+            CharacterPool.Initiate(CharacterType.TieShan, 0,
+                              () => new(GameData.CharacterRadius, CharacterType.TieShan, MoneyPool));
+            CharacterPool.Initiate(CharacterType.ZhiZhujing, 0,
+                              () => new(GameData.CharacterRadius, CharacterType.ZhiZhujing, MoneyPool));
             CharacterPool.Initiate(CharacterType.Pawn, 0,
                               () => new(GameData.CharacterRadius, CharacterType.Pawn, MoneyPool));
         }
