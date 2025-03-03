8911ce1b0fd39ef45c20b7975b630a11d9702e42
Xyberoid <143863451+Xyberoid@users.noreply.github.com>
2025-03-03 22:59:24 +0800

    fix: HomeFix

--- logic/GameClass/GameObj/Areas/Home.cs
+++ logic/GameClass/GameObj/Areas/Home.cs
@@ -10,7 +10,7 @@ public class Home(XY initPos, long id, int sideFlag)
     : Immovable(initPos, GameData.NumOfPosGridPerCell / 2, GameObjType.Home), IHome
 {
     public long TeamID { get; } = id;
-    sideFlag = sideFlag;
+    public int sideFlag = sideFlag;
     public InVariableRange<long> HP { get; } = new(GameData.HomeHP);
     public override bool IsRigid(bool args = false) => true;
     public override ShapeType Shape => ShapeType.Square;
