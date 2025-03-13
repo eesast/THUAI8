6efb115e5f7aa6479fdb635e78502d958d53d836
Henry <2280825018@qq.com>
2025-03-13 22:12:00 +0800

    fix: Debug in server

--- logic/GameClass/GameObj/Areas/E_Resource.cs
+++ logic/GameClass/GameObj/Areas/E_Resource.cs
@@ -1,6 +1,7 @@
 using Preparation.Utility;
 using Preparation.Utility.Value;
 using Preparation.Utility.Value.SafeValue.LockedValue;
+using System.Threading.Tasks.Dataflow;
 
 namespace GameClass.GameObj.Areas;
 
@@ -10,8 +11,20 @@ public class E_Resource(XY initPos)
     public InVariableRange<long> HP { get; } = new(GameData.ResourceHP);
     public override bool IsRigid(bool args = false) => false;
     public override ShapeType Shape => ShapeType.NULL_SHAPE_TYPE;
+    protected readonly object actionLock = new();
+    public object ActionLock => actionLock;
     public bool Produce(int producespeed, Character character)
     {
         return character.MoneyPool.AddMoney(-HP.SubRChange(producespeed)) > 0;
     }
+    private EconomyResourceState State = EconomyResourceState.NULL_ECONOMY_RESOURCE_STATE;
+    public EconomyResourceState ERstate
+    {
+        get
+        {
+            lock (actionLock)
+                return State;
+        }
+    }
+    public A_ResourceType AResourceType { get; }
 }
