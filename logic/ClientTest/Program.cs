ad49a0a4bb7ee2747dbb9c7fbaf760fafd46d32f
hhzzff <2070620600@qq.com>
2025-04-02 22:11:38 +0800

    feat: :rocket: run_all.cmd

--- logic/ClientTest/Program.cs
+++ logic/ClientTest/Program.cs
@@ -7,14 +7,25 @@ namespace ClientTest
     {
         public static Task Main(string[] args)
         {
+            // 从命令行参数解析 CharacterId 和 TeamId
+            if (!int.TryParse(args[0], out int characterId)) {
+                Console.WriteLine("Invalid CharacterId. Please provide a valid integer.");
+                return Task.CompletedTask;
+            }
+
+            if (!int.TryParse(args[1], out int teamId)) {
+                Console.WriteLine("Invalid TeamId. Please provide a valid integer.");
+                return Task.CompletedTask;
+            }
             Thread.Sleep(3000);
             Channel channel = new("127.0.0.1:8888", ChannelCredentials.Insecure);
             var client = new AvailableService.AvailableServiceClient(channel);
             CharacterMsg playerInfo = new()
             {
-                CharacterId = 0,
-                TeamId = 0,
-                CharacterType = CharacterType.TangSeng
+                CharacterId = characterId,
+                TeamId = teamId,
+                CharacterType = teamId == 0 ? CharacterType.JiuLing : CharacterType.TangSeng,
+                SideFlag = 1 - teamId
             };
             var call = client.AddCharacter(playerInfo);
             MoveMsg moveMsg = new()
@@ -33,10 +44,10 @@ namespace ClientTest
             while (true)
             {
                 Thread.Sleep(50);
-                MoveRes boolRes = client.Move(moveMsg);
+                //MoveRes boolRes = client.Move(moveMsg);
                 //if (boolRes.ActSuccess == false) break;
                 tot++;
-                if (tot % 10 == 0) moveMsg.Angle += 1;
+                //if (tot % 10 == 0) moveMsg.Angle += 1;
             }
             return Task.CompletedTask;
         }
