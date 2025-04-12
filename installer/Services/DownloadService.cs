84e620a90ce176f8f8c8325fefd69fab5f90a23b
frieren violet <FrierenViolet@outlook.com>
2025-04-12 19:44:43 +0800

    docs:

--- installer/Services/DownloadService.cs
+++ installer/Services/DownloadService.cs
@@ -1,117 +1,80 @@
 using System;
-using System.IO;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Collections.Generic;
-using CommunityToolkit.Mvvm.ComponentModel;
+using System.IO;
 
 namespace installer.Services
 {
-    public class DownloadService : ObservableObject
+    public class DownloadService
     {
         private readonly HttpClient _httpClient;
-        private CancellationTokenSource? _cancellationTokenSource;
-        private bool _isPaused;
-        private readonly object _lockObject = new object();
-
+        
         public DownloadService()
         {
             _httpClient = new HttpClient();
         }
-
-        public async Task DownloadFileAsync(string url, string filePath, IProgress<double> progress)
+        
+        public async Task DownloadFileAsync(string url, string filePath, IProgress<double> progress = null)
         {
-            _cancellationTokenSource = new CancellationTokenSource();
-            _isPaused = false;
-
+            var tempFilePath = filePath + ".temp";
+            
             try
             {
-                var fileInfo = new FileInfo(filePath);
-                var directory = fileInfo.Directory;
-                if (!directory.Exists)
-                {
-                    directory.Create();
-                }
-
-                var tempFilePath = filePath + ".tmp";
-                var downloadPosition = 0L;
-
+                // 确保目标目录存在
+                Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+                
+                // 删除任何已存在的临时文件
                 if (File.Exists(tempFilePath))
                 {
-                    downloadPosition = new FileInfo(tempFilePath).Length;
+                    File.Delete(tempFilePath);
                 }
-
-                using (var response = await _httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead, _cancellationTokenSource.Token))
+                
+                using (var response = await _httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead))
                 {
                     response.EnsureSuccessStatusCode();
-                    var totalBytes = response.Content.Headers.ContentLength ?? -1L;
-
-                    using (var contentStream = await response.Content.ReadAsStreamAsync(_cancellationTokenSource.Token))
+                    
+                    var totalBytes = response.Content.Headers.ContentLength ?? -1;
+                    
+                    using (var contentStream = await response.Content.ReadAsStreamAsync())
                     using (var fileStream = new FileStream(tempFilePath, FileMode.Append, FileAccess.Write, FileShare.None))
                     {
                         var buffer = new byte[8192];
                         var bytesRead = 0L;
-
+                        
                         while (true)
                         {
-                            while (_isPaused)
-                            {
-                                await Task.Delay(100, _cancellationTokenSource.Token);
-                            }
-
-                            var read = await contentStream.ReadAsync(buffer, 0, buffer.Length, _cancellationTokenSource.Token);
+                            var read = await contentStream.ReadAsync(buffer, 0, buffer.Length);
                             if (read == 0) break;
-
-                            await fileStream.WriteAsync(buffer, 0, read, _cancellationTokenSource.Token);
+                            
+                            await fileStream.WriteAsync(buffer, 0, read);
                             bytesRead += read;
-
+                            
                             if (totalBytes != -1)
                             {
                                 var progressValue = (double)bytesRead / totalBytes;
-                                progress.Report(progressValue);
+                                progress?.Report(progressValue);
                             }
                         }
                     }
                 }
-
+                
                 if (File.Exists(filePath))
                 {
                     File.Delete(filePath);
                 }
-
+                
                 File.Move(tempFilePath, filePath);
             }
-            catch (OperationCanceledException)
+            catch
             {
-                // 下载被取消
+                // 下载失败，清理临时文件
+                if (File.Exists(tempFilePath))
+                {
+                    File.Delete(tempFilePath);
+                }
+                throw;
             }
-            finally
-            {
-                _cancellationTokenSource?.Dispose();
-                _cancellationTokenSource = null;
-            }
-        }
-
-        public void Pause()
-        {
-            lock (_lockObject)
-            {
-                _isPaused = true;
-            }
-        }
-
-        public void Resume()
-        {
-            lock (_lockObject)
-            {
-                _isPaused = false;
-            }
-        }
-
-        public void Cancel()
-        {
-            _cancellationTokenSource?.Cancel();
         }
     }
 }
