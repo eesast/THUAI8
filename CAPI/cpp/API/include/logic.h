808e7eb2e3eb1fd5b17203d1dc8ebc103d518631
王一涵 <1572542354@qq.com>
2025-05-11 21:15:20 +0800

    fix: :bug: 关于EconomyResource、AdditionResource、Construction信息获取的改进，另外还添加了AttackConstruction、AttackAdditionResource函数，但尚未完成

--- CAPI/cpp/API/include/logic.h
+++ CAPI/cpp/API/include/logic.h
@@ -98,8 +98,9 @@ private:
     [[nodiscard]] std::shared_ptr<const THUAI8::GameInfo> GetGameInfo() const;
     [[nodiscard]] std::vector<int64_t> GetPlayerGUIDs() const;
     [[nodiscard]] THUAI8::PlaceType GetPlaceType(int32_t cellX, int32_t cellY) const;
-    [[nodiscard]] std::optional<THUAI8::EconomyResourceState> GetEnconomyResourceState(int32_t cellX, int32_t cellY) const;
-    [[nodiscard]] std::optional<std::pair<int32_t, int32_t>> GetAdditionResourceState(int32_t cellX, int32_t cellY) const;
+    [[nodiscard]] std::optional<THUAI8::EconomyResource> GetEconomyResourceState(int32_t cellX, int32_t cellY) const;
+    [[nodiscard]] std::optional<THUAI8::AdditionResource> GetAdditionResourceState(int32_t cellX, int32_t cellY) const;
+    [[nodiscard]] std::optional<THUAI8::ConstructionState> GetConstructionState(int32_t cellX, int32_t cellY) const;
     [[nodiscard]] int32_t GetEnergy() const;
     [[nodiscard]] int32_t GetScore() const;
 
@@ -115,6 +116,7 @@ private:
     bool Move(int64_t moveTimeInMilliseconds, double angle);
     bool Skill_Attack(int64_t playerID, int64_t teamID, double angleInRadian);
     bool Common_Attack(int64_t playerID, int64_t teamID, int64_t ATKplayerID, int64_t ATKteamID);
+    bool AttackConstruction(int64_t teamID, int64_t playerID);
     bool Recover(int64_t recover);
     bool Produce(int64_t playerID, int64_t teamID);
     // bool Rebuild(THUAI8::ConstructionType constructionType);
